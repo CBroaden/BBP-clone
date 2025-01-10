da640d93fc010dd4f0cd2aab3e7b642c86f4fb4c
Carl Broaden <carlbroaden@gmail.com>
2025-01-10 03:09:56 -0500

    fixed bug with image format name

--- app/components/carousel1.tsx
+++ app/components/carousel1.tsx
@@ -7,13 +7,13 @@ import Link from "next/link";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import 'swiper/css'
 
-const slides = ['/slides1/pt1.jpg','/slides1/pt2.jpg','/slides1/pt3.jpg','/slides1/pt4.jpg','/slides1/pt5.jpg','/slides1/pt6.jpg','/slides1/pt7.jpg','/slides1/pt8.jpg','/slides1/pt9.jpg','/slides1/pt10.jpg','/slides1/pt11.jpg','/slides1/pt12.jpg','/slides1/pt13.jpg','/slides1/pt14.jpg']
+const slides = ['/slides1/pt1.jpg','/slides1/pt2.jpg','/slides1/pt3.jpg','/slides1/pt4.jpg','/slides1/pt5.jpg','/slides1/pt6.jpg','/slides1/pt7.JPG','/slides1/pt8.jpg','/slides1/pt9.jpg','/slides1/pt10.jpg','/slides1/pt11.jpg','/slides1/pt12.jpg','/slides1/pt13.jpg','/slides1/pt14.jpg']
 
 
 export default function Carousel1() {
   return (
     
-        <div className="border-y-2 w-[98vw] mx-auto border-black bg-zinc-300 ">
+        <div className="border-y-2 w-[98vw] mx-auto border-black bg-zinc-300">
         <Swiper
           className=" my-1 h-[25vw]"
           spaceBetween={1}
