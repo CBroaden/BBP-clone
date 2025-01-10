28ff12a46d0c86aeb5daecd395da03a8c1b325fd
Carl Broaden <carlbroaden@gmail.com>
2025-01-10 01:13:37 -0500

    boldened business name in header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -7,7 +7,7 @@ export default function Header() {
         <div className=" flex flex-col max-w-6xl md:flex-row justify-between items-center mx-auto w-full bg-gradient-radial from-transparent from-25% via-fuchsia-200 via-45% to-65% to-transparent">
           <Image height={350} width={350} alt="Three Balloons" src='/BellasBalloons.svg' className="mx-auto my-auto"/>
           <div className="md:w-[30rem] w-4/5 mx-auto my-auto flex flex-col gap-4">
-            <h1 className='font-marker w-full text-center md:text-left text-xl'>Bella&apos;s Balloons & Backdrops</h1>
+            <h1 className='font-marker w-full text-center md:text-left text-xl font-bold'>Bella&apos;s Balloons & Backdrops</h1>
             <p className='font-roboto mx-auto text-lg'>Elevate your event with our stunning photo backdrops and balloon decorations. We add a touch of <span className='animated-gradient font-marker italic bg-gradient-to-r pr-1 from-[aquamarine] via-yellow-500 to-[fuchsia] text-transparent bg-clip-text font-black'>Magic</span> to your celebrations. Let&apos;s make your moments unforgettable!</p>
             <Link href="/booking" className="mx-auto md:ml-0">
               <button className="button ">Order Now!</button>
